aad66a4a89de1f38f9ded64e05df512421340cd5
Sema Nur ekmekci <ekmekci.semanur@gmail.com>
2023-07-17 22:52:07 +0300

    Add files via upload

---
+++ LoginValidation.js
@@ -0,0 +1,27 @@
+// function Validation(values) {
+//     let errors = {};
+//     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     // const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d\W]{5,}$/;
+  
+//     if (values.email === "") {
+//       errors.email = "Email alanı boş bırakılamaz";
+//     } else if (!emailPattern.test(values.email)) {
+//       errors.email = "Geçerli bir email adresi girin";
+//     } else {
+//       errors.email = "";
+//     }
+  
+    // if (values.password === "") {
+    //   errors.password = "Şifre alanı boş bırakılamaz";
+    // } else if (!passwordPattern.test(values.password)) {
+    //   errors.password =
+    //     "Şifre en az 5 karakter uzunluğunda olmalı ve en az bir büyük harf, bir küçük harf ve bir rakam içermelidir";
+    // } else {
+    //   errors.password = "";
+    // }
+  
+  //   return errors;
+  // }
+  
+  // export default Validation;
+  
---
+++ SignupValidation.js
@@ -0,0 +1,33 @@
+function Validation(values) {
+    let errors = {};
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d\W]{5,}$/;
+  
+    if (!values.name || values.name === "") {
+      errors.name = "Kullanıcı adı alanı boş bırakılamaz";
+    }else {
+      errors.name = "";
+    }
+      
+    if (values.email === "") {
+        errors.email = "Email alanı boş bırakılamaz";
+      } else if (!emailPattern.test(values.email)) {
+        errors.email = "Geçerli bir email adresi girin";
+      } else {
+        errors.email = "";
+      }
+  
+    if (values.password === "") {
+      errors.password = "Şifre alanı boş bırakılamaz";
+    } else if (!passwordPattern.test(values.password)) {
+      errors.password =
+        "Şifre en az 5 karakter uzunluğunda olmalı ve en az bir büyük harf, bir küçük harf ve bir rakam içermelidir";
+    } else {
+      errors.password = "";
+    }
+  
+    return errors;
+  }
+  
+  export default Validation;
+  
